918d52f6e64fa718ae5750f1176454d5947bd713
gelsyjuntilla <g.juntilla@klab.com>
2015-09-18 10:56:40 +0800

    Created getComments() function

--- board/app/models/thread.php
+++ board/app/models/thread.php
@@ -30,5 +30,19 @@
     		}
     		return new self($row);
     	}
+
+    	public function getComments()
+    	{
+    		$comments = array();
+
+    		$db = DB::conn();
+
+    		$rows = $db->rows('SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC', array($this->id));
+
+    		foreach ($rows as $row) {
+    			$comments[] = new Comments($row);
+    		}
+    		return $comments
+    	}
     }
 ?>
