17d750d42b2abcb3b88341a4ebc1cb2dc747f1c0
gelsyjuntilla <g.juntilla@klab.com>
2015-09-18 11:41:14 +0800

    Created a write() method for comments

--- board/app/models/thread.php
+++ board/app/models/thread.php
@@ -37,12 +37,20 @@
 
     		$db = DB::conn();
 
-    		$rows = $db->rows('SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC', array($this->id));
+    		$rows = $db->rows('SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC', 
+    			                array($this->id));
 
     		foreach ($rows as $row) {
     			$comments[] = new Comment($row);
     		}
     		return $comments;
+    	}
+
+    	public function write(Comment $comment)
+    	{
+    		$db = DB::conn();
+    		$db->query('INSERT INTO comment SET thread_id = ?, username = ?, body = ?, created = NOW()', 
+    			    array($this->id, $comment->username, $comment->body));
     	}
     }
 ?>
