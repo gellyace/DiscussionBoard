969f6616f731bccd652393ef3a6b7da3e0217758
gelsyjuntilla <g.juntilla@klab.com>
2015-09-18 14:52:31 +0800

    Add validation rule to the comment model

--- board/app/models/comment.php
+++ board/app/models/comment.php
@@ -4,5 +4,8 @@
     */
     class Comment extends AppModel
     {
-    	    	
+        public $validation = array(
+                                'username' => array('length' => array('validate_between', 1 ,16),), 
+                                'body' => array('length' => array('validate_between', 1 ,50),), 
+                                );
     }
