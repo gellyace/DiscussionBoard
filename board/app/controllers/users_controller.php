29d8ef2212bd8852c16598594ef234faec3fd607
gelsyjuntilla <g.juntilla@klab.com>
2015-09-21 17:06:51 +0800

    Controller Class for Users

---
+++ board/app/controllers/users_controller.php
@@ -0,0 +1,13 @@
+<?php
+class UsersController extends AppController
+{
+    public function index() // login home page
+    {
+        
+    }
+
+    public function register() // register page
+    {
+    	
+    }
+}
