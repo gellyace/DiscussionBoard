f31020c8522ba2365b1ffc238c78d0dc719892fd
gelsyjuntilla <g.juntilla@klab.com>
2015-09-22 11:09:58 +0800

    Added register method/function

--- board/app/controllers/users_controller.php
+++ board/app/controllers/users_controller.php
@@ -8,6 +8,31 @@ class UsersController extends AppController
 
     public function register() // register page
     {
-    	
+    	$user = new Users;
+        $page = Param::get('page_next', 'register');
+
+        switch ($page) {
+            case 'register':
+                break;
+                
+            case 'register_end':
+                $user->username = Param::get('username');
+                $user->firstname=Param::get('firstname');
+                $user->lastname=Param::get('lastname');
+                $user->email=Param::get('email');
+                $user->password=Param::get('password');
+                try {
+                    $user->register($user);
+                } catch (ValidationException $e) {
+                    $page='register';
+                }
+                    break;
+
+            default:
+                throw new NotFoundException("{$page} is not found");                    
+                break;
+        }
+            $this->set(get_defined_vars());
+            $this->render($page);
     }
 }
