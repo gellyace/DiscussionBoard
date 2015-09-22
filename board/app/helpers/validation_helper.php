65ae1efce8c3198071f709dcd05555d50fe9001b
gelsyjuntilla <g.juntilla@klab.com>
2015-09-22 16:34:59 +0800

    Added some abstract helper functions

--- board/app/helpers/validation_helper.php
+++ board/app/helpers/validation_helper.php
@@ -5,3 +5,17 @@
 
     	return $min <= $n && $n <= $max;
     }
+
+    function validate_name($check, $name){
+
+    }
+
+    function validate_username_password($name){
+    	if(ctype_alnum($name))
+    	    return $name;
+    }
+
+    function validate_email()
+    {
+    	
+    }
