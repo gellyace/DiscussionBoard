71f2b5680b40bfe00f28228297ca95c9bb7f43e3
gelsyjuntilla <g.juntilla@klab.com>
2015-09-23 15:46:27 +0800

    made some changes

--- board/app/views/users/index.php
+++ board/app/views/users/index.php
@@ -2,30 +2,38 @@
 <html>
 <head> 
 	<title>Login Page</title>
-	
 </head>
-
 <body>
-<h2>Welcome Guest</h2>
+
+<!-- Wrong credentials of user-->
+<?php if (!$user->user_validated): ?>
+	<div class="alert alert-error">
+		<em><h4 class="alert-heading">Wait!</h4></em>
+		<em>Invalid Username or Password</em>
+	</div>
+<?php endif ?>
 
 
-    <form name="login" method="post" action="<?php eh(url('thread/index')) ?>">
-		<fieldset>
-		<legend>User Login</legend>
-		<div>
+<div id="header"> <h2>Welcome Guest</h2> </div>
+
+    <form id="body" method="post" action="<?php eh(url('')) ?>">
+		<div id="rcorners2">
 			<label>Username</label>
 			<input type="text" class="span2" name="username" placeholder="username" value="<?php eh(Param::get('username')) ?>" />
 			
 			<label>Password</label>
 			<input type="password" class="span2" name="password" placeholder="password" value="<?php eh(Param::get('password')) ?>" />
 
-			<br/>			
+			<br/>
+			<!-- Redirection to home, if logging in is succesful-->		
+			<input type="hidden" name="page_next" value="index_end">	
 			<button type="submit" class="btn btn-primary"> Login </button>
-
-			<br/><br/> Don't have an account? <a href="<?php eh(url('users/register')) ?>"> Register here </a>
+			<br/><br/> 
+			<!-- Once hyperlink is clicked, will be redirected to Register Page-->
+			Don't have an account? <a href="<?php eh(url('users/register')) ?>"> Register here </a>
 		</div>
-		</fieldset>
 	</form>
+
 
 </body>
 </html>
