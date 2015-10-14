55f9e6b16a61e87afefaabe6bc832be4764599ac
gelsyjuntilla <g.juntilla@klab.com>
2015-10-14 11:37:46 +0800

    improved the redirection

--- board/app/controllers/likes_controller.php
+++ board/app/controllers/likes_controller.php
@@ -3,23 +3,25 @@ class LikesController extends AppController
 {
     public function like() 
     {
-		check_user_session(get_session_username());
-		$thread_id = Param::get('thread_id');
-		$comment_id = Param::get('comment_id');
-		$user_id = get_session_id();
+        check_user_session(get_session_username());
 
-		Likes::like($user_id, $comment_id, $thread_id);
- 		redirect('/comment/view?thread_id='.$thread_id);
-	}
+        $thread_id = Param::get('thread_id');
+        $comment_id = Param::get('comment_id');
+        $user_id = get_session_id();
 
-	public function unlike() 
-	{
-		check_user_session(get_session_username());
-		$thread_id = Param::get('thread_id');
-		$comment_id = Param::get('comment_id');
-		$user_id = get_session_id();
-		
-		Likes::unlike($user_id, $comment_id, $thread_id);
-		redirect('/comment/view?thread_id='.$thread_id);
-	}
+        Likes::like($user_id, $comment_id, $thread_id);
+        redirect(url('comment/view', array('thread_id' => $thread_id)));
+    }
+
+    public function unlike() 
+    {
+        check_user_session(get_session_username());
+        
+        $thread_id = Param::get('thread_id');
+        $comment_id = Param::get('comment_id');
+        $user_id = get_session_id();
+        
+        Likes::unlike($user_id, $comment_id, $thread_id);
+        redirect(url('comment/view', array('thread_id' => $thread_id)));
+    }
 }
